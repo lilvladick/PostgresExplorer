cfb77b1f3fa3e0c884d21951e3f6d2140e7187ad
Владислав <103439608+lilvladick@users.noreply.github.com>
2025-09-03 18:09:42 +0300

    Initial Commit

---
+++ PostgresExplorer/PostgresExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostgresExplorerApp.swift
+//  PostgresExplorer
+//
+//  Created by Владислав Кириллов on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PostgresExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
